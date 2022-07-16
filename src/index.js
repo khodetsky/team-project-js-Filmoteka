0740786c8cd2728102fc952f8ae56d9778f923b3
khodetsky <hodyaya@gmail.com>
2022-07-16 20:56:21 +0300

    delete fetch trending films

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 // Основний файл першої сторінки. Сюди будуть імпортуватися окремі функції з ./js/index
 // Додавайте ці окремі файли саме у ту папку і експортуйте сюди.
-import {createArrOfGenres} from './js/index/trending-fetch';
-import { getMoviesMcp } from './js/index/trending-fetch';
-
-getMoviesMcp();
