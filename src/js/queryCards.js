5bd2ed6bee523f9ea87e399c9881b19e7c2c2927
SashaMaslak <owmaslak@gmail.com>
2022-07-19 15:31:21 +0300

    fix input-trim and incorrect query

--- src/js/queryCards.js
+++ src/js/queryCards.js
@@ -4,27 +4,31 @@ import { target, spinner } from './spinner.js';
 
 const gallery = document.querySelector('.gallery-home');
 const formSearch = document.querySelector('#search-form');
+const textError = document.querySelector('.header__text-error');
 
 formSearch.addEventListener('submit', submitMoviesSearch);
 let searchMovie = '';
 
 async function submitMoviesSearch(e) {
+   textError.classList.add('is-hidden');
    spinner.spin(target);
    e.preventDefault();
    window.scrollTo(0, 0);
    resetPage();
 
-   searchMovie = formSearch.elements.searchQuery.value;
+   searchMovie = (formSearch.elements.searchQuery.value).trim();;
 
    if (searchMovie === '') {
       spinner.stop();
-      return console.log('NOTIFICATION => Empty field');  // Поставити Нотіфікашку
+      textError.classList.remove('is-hidden');
+      return console.log('No matches found for your query. Enter the correct movie name.');
    }
    
    await fetchQueryMovies(searchMovie).then(({ movies }) => {
       if (movies.length === 0) {
          spinner.stop();
-         return console.log('NOTIFICATION => No query results');  // Поставити Нотіфікашку
+         textError.classList.remove('is-hidden');
+         return console.log('The search result is unsuccessful. Enter the correct movie title');  // Поставити Нотіфікашку
       }
       gallery.innerHTML = createMovieCards(movies);
    })
