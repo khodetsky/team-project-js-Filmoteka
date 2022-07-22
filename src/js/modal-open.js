884ef029cc394bf46e9f3a7cd45d417c01f4b013
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-07-22 14:15:54 +0300

    fixed

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -8,26 +8,26 @@ import {
 } from './modal-close';
 import { genres } from './genres.json';
 const gallery = document.querySelector('.gallery');
-const poster = document.querySelector('#modal-img');
-const filmName = document.querySelector('.modal__data--title');
-const vote = document.querySelector('#modal-vote');
-const votes = document.querySelector('#modal-votes');
-const popularity = document.querySelector('#modal-popularity');
-const originTitle = document.querySelector('#modal-orgtitle');
-const ganre = document.querySelector('#modal-genre');
-const about = document.querySelector('#modal-p');
-let card;
-let id;
-let filmData = {};
+// const poster = document.querySelector('#modal-img');
+// const filmName = document.querySelector('.modal__data--title');
+// const vote = document.querySelector('#modal-vote');
+// const votes = document.querySelector('#modal-votes');
+// const popularity = document.querySelector('#modal-popularity');
+// const originTitle = document.querySelector('#modal-orgtitle');
+// const ganre = document.querySelector('#modal-genre');
+// const about = document.querySelector('#modal-p');
+// let card;
+// let id;
+// let filmData = {};
 
 // вішаємо слухача для відкриття модалки з повною інформацією про фільм
 gallery.addEventListener('click', e => {
   const onClickElement = e.target;
-  card = onClickElement.closest('.card');
+  const card = onClickElement.closest('.card');
   if ((card != null) & backdrop.classList.contains('hidden')) {
     id = card.dataset.id;
     modalVisible();
-    getFilmData(id);
+    // getFilmData(id);
     setTimeout(() => {
       modalClose();
     }, 100);
@@ -53,27 +53,27 @@ function modalClose() {
 }
 
 // підгрузка інформації про фільм в модалку
-function getFilmData(id) {
-  const filmsData = JSON.parse(localStorage.getItem('moviesInMain'));
-  console.log(`Шукаємо дані про фільм в LocalStorage по id фільму ${id}`);
-  filmsData.forEach(film => {
-    if (film.id == id) {
-      filmData = film;
-      modalRender(filmData);
-    } else {
-      return;
-    }
-  });
-}
+// function getFilmData(id) {
+//   const filmsData = JSON.parse(localStorage.getItem('moviesInMain'));
+//   console.log(`Шукаємо дані про фільм в LocalStorage по id фільму ${id}`);
+//   filmsData.forEach(film => {
+//     if (film.id == id) {
+//       filmData = film;
+//       modalRender(filmData);
+//     } else {
+//       return;
+//     }
+//   });
+// }
 
-function modalRender(film) {
-  console.log('заповнюємо модалку інформацією про фільм');
-  poster.src = `https://image.tmdb.org/t/p/w500/${film.poster_path}`;
-  filmName.textContent = film.title;
-  vote.textContent = film.vote_average;
-  votes.textContent = film.vote_count;
-  popularity.textContent = film.popularity;
-  originTitle.textContent = film.original_title;
-  // const ganre = film.;
-  about.textContent = film.overview;
-}
+// function modalRender(film) {
+//   console.log('заповнюємо модалку інформацією про фільм');
+//   poster.src = `https://image.tmdb.org/t/p/w500/${film.poster_path}`;
+//   filmName.textContent = film.title;
+//   vote.textContent = film.vote_average;
+//   votes.textContent = film.vote_count;
+//   popularity.textContent = film.popularity;
+//   originTitle.textContent = film.original_title;
+//   // const ganre = film.;
+//   about.textContent = film.overview;
+// }
