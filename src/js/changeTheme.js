113d6d5072ced99d1dd8ae27bbf780860d2eb6b0
SashaMaslak <owmaslak@gmail.com>
2022-07-23 12:51:42 +0300

    fix theme

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,6 +1,15 @@
 const body = document.querySelector('body');
 const btnLightTheme = document.querySelector('.theme__btn--light');
 const btnDarkTheme = document.querySelector('.theme__btn--dark');
+const footer = document.querySelector('footer');
+const modal = document.querySelector('.modal');
+const modalFooter = document.querySelector('.footer-modal');
+const modalDataElementWhite = document.querySelector('.modal__data--element--white');
+const footerModalText = document.querySelector('.footer-modal__text');
+
+
+
+
 
 btnLightTheme.addEventListener('click', setLightTheme);
 btnDarkTheme.addEventListener('click', setDarkTheme);
@@ -8,7 +17,6 @@ btnDarkTheme.addEventListener('click', setDarkTheme);
 let currentTheme = '';
 
 if (localStorage.getItem('currentTheme') === 'dark') {
-   console.log('currentTheme = dark');
    setDarkTheme();
 }
 
@@ -16,6 +24,11 @@ function setLightTheme() {
    btnDarkTheme.style.display = "block";
    btnLightTheme.style.display = "none";
    body.classList.remove('dark');
+   footer.classList.remove('dark');
+   modalFooter.style.backgroundColor = "white";
+   footerModalText.style.color = "#545454";
+   modal.style.backgroundColor = "white";
+   modalDataElementWhite.style.backgroundColor = "white";
    currentTheme = localStorage.setItem('currentTheme', 'light');
 }
 
@@ -23,6 +36,11 @@ function setDarkTheme() {
    btnLightTheme.style.display = "block";
    btnDarkTheme.style.display = "none";
    body.classList.add('dark');
+   footer.classList.add('dark');
+   modalFooter.style.backgroundColor = "#333";
+   footerModalText.style.color = "white";
+   modal.style.backgroundColor = "#333";
+   modalDataElementWhite.style.backgroundColor = "transparent";
    currentTheme = localStorage.setItem('currentTheme', 'dark');
 }
 
